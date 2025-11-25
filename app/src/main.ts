908a8d0729ba254762fb8232a550592244e91a10
eageev <eageev@kozhin.dev>
2025-11-25 16:47:50 +0300

    feat: add swagger docs, update dependencies

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,13 +5,46 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
       AppModule,
       new FastifyAdapter(),
   );
 
+  await app.register(swagger, {
+    swagger: {
+      info: {
+        title: 'SID-based Anti-Fraud API',
+        description: 'API documentation for anti-fraud system',
+        version: '1.0.0',
+      },
+      consumes: ['application/json'],
+      produces: ['application/json'],
+    },
+  });
+
+  await app.register(swaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: true,
+    },
+  });
+
+  const config = new DocumentBuilder()
+      .setTitle('SID Anti-Fraud API')
+      .setDescription('Documentation for SID-based Anti-Fraud service')
+      .setVersion('0.1')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/nest-docs', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
-
-void bootstrap();
+bootstrap();
